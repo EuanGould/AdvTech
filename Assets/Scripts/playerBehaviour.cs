b7d092f25c3b329b3b51b45a15a21a508001192c
Euan <euan2.gould@live.uwe.ac.uk>
2025-10-28 17:37:22 +0000

    proximity line

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -5,18 +5,22 @@ using Unity.Netcode;
 public class playerBehaviour : NetworkBehaviour
 {
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float chat_length = 10f;
 
     private Vector3 respawnPoint;
 
     private Rigidbody rb;
 
     private Vector2 movingDirection = Vector2.zero;
+
+    private LineRenderer lr;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         respawnPoint = transform.position;
+        lr = gameObject.GetComponent<LineRenderer>();
 
     }
 
@@ -34,6 +38,19 @@ public class playerBehaviour : NetworkBehaviour
             rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0) + (new Vector3(movingDirection.x, 0, movingDirection.y) * speed);
         }
 
+        if (GameObject.FindGameObjectsWithTag("Player").Length != 2)
+        {
+            LineDefault();
+        }
+        else if ((GameObject.FindGameObjectsWithTag("Player")[0].transform.position - GameObject.FindGameObjectsWithTag("Player")[1].transform.position).magnitude < chat_length)
+        {
+            lr.SetPosition(0, GameObject.FindGameObjectsWithTag("Player")[0].transform.position);
+            lr.SetPosition(1, GameObject.FindGameObjectsWithTag("Player")[1].transform.position);
+        }
+        else
+        {
+            LineDefault();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,5 +59,11 @@ public class playerBehaviour : NetworkBehaviour
         {
             gameObject.transform.position = respawnPoint;
         }
+    }
+
+    private void LineDefault()
+    {
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, transform.position);
     }
 }
