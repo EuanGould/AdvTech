9994ff04e5ea2a47b76e08511a0745a11fced50f
Eunie <euan2.gould@live.uwe.ac.uk>
2025-11-20 10:45:48 +0000

    second level begins, buttons and lights

---
+++ Assets/Scripts/lightButtonBehaviour.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightButtonBehaviour : MonoBehaviour
+{
+    [SerializeField] Material on;
+    [SerializeField] Material off;
+    [SerializeField] float compressed_y_size;
+    [SerializeField] int max_charges = 4;
+
+    private GameObject light_object;
+
+    private Vector3 default_scale;
+
+    private AudioSource audio_source;
+
+    private float light_timer = 0;
+
+    private bool light_status = false;
+
+    private int charges;
+
+    private void Awake()
+    {
+        audio_source = GetComponent<AudioSource>();
+        default_scale = transform.localScale;
+        light_object = transform.GetChild(0).gameObject;
+        charges = max_charges;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (charges > 0)
+            {
+                transform.localScale = new Vector3(1, compressed_y_size, 1);
+                audio_source.Play();
+                light_timer = 0.1f;
+                light_status = true;
+                light_object.GetComponent<MeshRenderer>().material = on;
+                charges --;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.localScale = default_scale;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (light_status)
+        {
+            if (light_timer > 0)
+            {
+                light_timer -= Time.fixedDeltaTime;
+            }
+            else
+            {
+                light_status = false;
+                light_object.GetComponent<MeshRenderer>().material = off;
+            }
+        }
+    }
+}
